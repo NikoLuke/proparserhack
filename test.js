87e16c935485facacdc1c15422c9ac456bc98899
Niko <nikolaslyko@gmail.com>
2017-10-17 13:55:44 +0200

    костяк/основа

--- test.js
+++ test.js
@@ -4,26 +4,22 @@ casper.userAgent('Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Fi
 
 casper.start('http://yandex.ru/blogs');
 
-// casper.then(function() {
-//     this.echo(this.getCurrentUrl());
-// });
-
 casper.then(function() {
    this.fill('form[action="/blogs/search"]', { text: 'парк' }, true);
 });
 
-casper.wait(2000, function() {
+casper.wait(1000, function() {
     this.clickLabel('За сутки', 'span');
-    this.wait(2000, function() {
-        this.capture("ya3.png");
+    this.wait(1500, function() {
+        this.capture("ya1.png");
     });
 });
 
 var links = [],
     headers = [],
     descriptions = [],
-    obj = [],
-    date = [];
+    date = [],
+    obj = [];
 
 function getLinks() {
     var links = document.querySelectorAll('h2 a');
@@ -32,67 +28,65 @@ function getLinks() {
     });
 }
 
-casper.then(function() {
-    headers = this.getElementsInfo('h2 a').map(function(info) {
-        return info.text.trim();
-    });    
-    descriptions = this.getElementsInfo('.text-container').map(function(info) {
-        return info.text.trim();
-    });    
-    links = this.evaluate(getLinks);    
-});
-
-casper.then(function() {
-    date = this.getElementsInfo('.organic__outside-right').map(function(info) {
-        return info.text.trim();
-    });
-});
-
-function getLastdate() {
-    var newDate = new Date();
-    return newDate = newDate.getDate() - 1;
-}
-
-var yesterday = "вчера",
-    daybeforeyesterday = "позавчера";
-
-function getT() {
+function getD() {
+    var yesterday = "вчера",
+        daybeforeyesterday = "позавчера";
     var arr = [];
-    var arrray;
+    var array;
+    var day = new Date();
     for (var i = 0; i < date.length; i++) {
         if (date[i] === yesterday) {
-            var day = new Date();
-            var a = getLastdate() + '/' + (day.getMonth() + 1) + '/' + day.getFullYear();
-            arrray = arr.push(a);
+            var a = (day.getDate() - 1) + '/' + (day.getMonth() + 1) + '/' + day.getFullYear();
+            array = arr.push(a);
         }
         else if (date[i] === daybeforeyesterday) {
-            var b = new Date();
-            var c = (b.getDate() - 2) + '/' + (b.getMonth() + 1) + '/' + b.getFullYear();
-            arrray = arr.push(c);
+            var b = (day.getDate() - 2) + '/' + (day.getMonth() + 1) + '/' + day.getFullYear();
+            array = arr.push(b);
         }
         else {
-            var k = new Date();
-            var f = k.getDate() + '/' + (k.getMonth() + 1) + '/' + k.getFullYear();
-            arrray = arr.push(f);
+            var c = day.getDate() + '/' + (day.getMonth() + 1) + '/' + day.getFullYear();
+            array = arr.push(c);
         }
     }
     return arr;
 }
 
-var wasd = [];
 casper.then(function() {
-    wasd = getT();
+    headers = this.getElementsInfo('h2 a').map(function(info) {
+        return info.text.trim();
+    });
+
+    descriptions = this.getElementsInfo('.text-container').map(function(info) {
+        return info.text.trim();
+    });
+
+    date = this.getElementsInfo('.organic__outside-right').map(function(info) {
+        return info.text.trim();  
+    });
+});
+
+var dt = [];
+casper.then(function() {
+    links = this.evaluate(getLinks);    
+    dt = getD();
 });
 
 casper.then(function() {
     for (var i = 0; i < links.length; i++) {
-        obj[i] = headers[i] + '\n' + links[i] + '\n' + descriptions[i] + '\n' + wasd[i] + '\n';          
+        obj[i] = headers[i] + '\n' + links[i] + '\n' + descriptions[i] + '\n' + dt[i] + '\n';          
     }
 });
 
+casper.then(function() {
+    this.clickLabel('2', 'a');
+    this.wait(1000, function() {
+        this.capture("ya2.png");
+    });
+});
+
 casper.run(function() {
+    this.echo(obj.length + ' founded: ' + '\n');
     this.echo(obj.join('\n')).exit();
-    // this.echo(getT()).exit();
     // this.echo(date.join('\n')).exit();
     // this.echo(headers.join('\n'));
     // this.echo(' - ' + descriptions.join('\n - '));
